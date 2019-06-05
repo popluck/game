8a9ceecaf193700fed438a48e3e2445183e97c0b
blessing <1424518876@qq.com>
2019-06-05 15:42:33 +0800

    Signed-off-by: blessing <1424518876@qq.com>

--- fishing/js/index.js
+++ fishing/js/index.js
@@ -1,15 +1,17 @@
-var wxdata = localStorage.getItem('wxdata')
-$.post(
-	"http://mq.soratech.cn/web_sock/public/index/index/user_insert",
-	user:wxdata,
-	function(res){
-		console.log(1)
+window.onload = function() {
+
+	$('.model_index')[0].addEventListener('click', dianji, false)
+	var wxdata = localStorage.getItem('wxdata')
+	$.post(
+		"http://mq.soratech.cn/web_sock/public/index/index/user_insert",{
+		user: wxdata,
+		},
+		function(res) {
+			console.log(1)
+		})
+
+	function dianji() {
+		$('.model_index')[0].style.display = 'none'
 	}
-)
 
-	$('.model_index')[0].addEventListener('click',dianji,false)
-			function dianji(){
-				$('.model_index')[0].style.display='none'
-			}
-
-		
+}
