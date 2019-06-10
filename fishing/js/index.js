621c66e3c498133ea1a9926bde92b0b720120625
blessing <1424518876@qq.com>
2019-06-10 17:48:55 +0800

    Signed-off-by: blessing <1424518876@qq.com>

--- fishing/js/index.js
+++ fishing/js/index.js
@@ -1,25 +1,34 @@
 //window.onload = function() {
+
 var btn = document.querySelector('.btn')
-			btn.addEventListener('touchstart',function(){
-				btn.style.transform='scale(0.8)'
-			})
-			btn.addEventListener('touchend',function(){
-				btn.style.transform='scale(1)'
-			})
-	$('.model_index')[0].addEventListener('click', dianji, false)
-	var wxdata = localStorage.getItem('wxdata')
-	alert(wxdata)
-	$.post(
-		"http://mq.soratech.cn/web_sock/public/index/index/user_insert",{
+btn.addEventListener('touchstart', function() {
+	btn.style.transform = 'scale(0.8)'
+})
+btn.addEventListener('touchend', function() {
+	btn.style.transform = 'scale(1)'
+})
+$('.model_index')[0].addEventListener('click', dianji, false)
+var wxdata = localStorage.getItem('wxdata')
+alert(wxdata)
+localStorage.setItem('room',GetQueryString('id'))
+alert(localStorage.getItem('room'))
+$.post(
+	"http://mq.soratech.cn/web_sock/public/index/index/user_insert", {
 		user: wxdata,
-		},
-		function(res) {
-			alert(res)
-			console.log(1)
-		})
+	},
+	function(res) {
+		alert(res)
+		console.log(1)
+	})
 
-	function dianji() {
-		$('.model_index')[0].style.display = 'none'
-	}
+function dianji() {
+	$('.model_index')[0].style.display = 'none'
+}
 
+function GetQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r != null) return unescape(r[2]);
+	return null;
+}
 //}
