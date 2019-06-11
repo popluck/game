3da97c4b64c0faeb95cdc1c44e57e1a036035cdc
blessing <1424518876@qq.com>
2019-06-11 18:44:56 +0800

    Signed-off-by: blessing <1424518876@qq.com>

--- fishing/js/index.js
+++ fishing/js/index.js
@@ -1,31 +1,30 @@
 // 钓鱼按钮
 var btn = document.querySelector('.btn')
-localStorage.setItem('room', GetQueryString('id'))
-alert(localStorage.getItem('room'))
-alert(localStorage.getItem('code'))
-var code=localStorage.getItem('code')
-$.get(
-					"http://mq.soratech.cn/stores/public/game/index/openid", {
-						code: code
-					},
-					function(res) {
-						console.log(res)
-						alert(res, 2)
-						localStorage.setItem('wxdata', res)
-						alert('微信信息”' + wxdata)
-
-					})
 btn.addEventListener('touchstart', function() {
 	btn.style.transform = 'scale(0.8)'
-}, false)
+})
 btn.addEventListener('touchend', function() {
 	btn.style.transform = 'scale(1)'
-}, false)
+})
 // 用户微信信息
+var wxdata = localStorage.getItem('wxdata')
+//alert(wxdata)
 // 门店id
-var imgURL = 0;
-var imgXS = 0
-var imgXS1 = 1
+localStorage.setItem('room',GetQueryString('id'))
+//alert(localStorage.getItem('room'))
+
+// 加入游戏
+$.post(
+	"http://mq.soratech.cn/web_sock/public/index/index/user_insert", {
+		user: wxdata,
+	},
+	function(res) {
+		alert(res)
+		console.log(1)
+	})
+var imgURL=0;
+var imgXS=0
+var imgXS1=1
 // 地址栏参数获取
 function GetQueryString(name) {
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -34,27 +33,23 @@ function GetQueryString(name) {
 	return null;
 }
 // 图片加载
-function imgload() {
-	//	console.log($('#bkg').css('background-image'))
-	//	$('.img1').eq(imgXS).css('background-image','url(image/index/shouye_'+imgURL+'.png)')
-	//	$('.img1').eq(imgXS+1).css('background-image','url(image/index/shouye_'+(imgURL+1)+'.png)')
-	//	$('.img1').eq(imgXS+2).css('background-image','url(image/index/shouye_'+(imgURL+2)+'.png)')
-	$('.img1').eq(imgXS1).css('background-image', 'url(image/index/shouye_' + (imgURL + 1) + '.png)')
-	imgURL = imgURL < 99 ? imgURL + 1 : 0
-	imgXS1 = imgXS1 < 3 ? imgXS1 + 1 : 0
+function imgload(){
+//	console.log($('#bkg').css('background-image'))
+//	$('.img1').eq(imgXS).css('background-image','url(image/index/shouye_'+imgURL+'.png)')
+//	$('.img1').eq(imgXS+1).css('background-image','url(image/index/shouye_'+(imgURL+1)+'.png)')
+//	$('.img1').eq(imgXS+2).css('background-image','url(image/index/shouye_'+(imgURL+2)+'.png)')
+	$('.img1').eq(imgXS1).css('background-image','url(image/index/shouye_'+(imgURL+1)+'.png)')
+ 
+	imgURL=imgURL<99?imgURL+1:0
 
 }
 //console.log($('.img1'))
 // 轮换
-function imgrotate() {
-	imgXS = imgXS < 3 ? imgXS + 1 : 0
-	for(var i = 0; i < 3; i++) {
-		$('.img1').eq(i).css('z-index', '-100')
-
-	}
-
-	$('.img1').eq(imgXS).css('z-index', '10')
-
+function imgrotate(){
+	imgXS = imgXS<3?imgXS+1:0
+	$('.img1').css('z-index','-100')
+	$('.img1').eq(imgXS).css('z-index','10')
+	
 }
 
 //	$('#bkg').css('background-image','url(image/index/shouye_'+imgURL+'.png)')
@@ -63,13 +58,4 @@ function imgrotate() {
 //	imgrotate()
 //	console.log(imgURL)
 //	console.log(imgXS)
-//},100)
-
-function start() {
-	imgload()
-	imgrotate()
-	console.log(imgURL)
-	console.log(imgXS)
-	requestAnimationFrame(start, Element)
-}
-//start()
+//},1000)
