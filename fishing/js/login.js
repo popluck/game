3bf8b79d063b2ad5c13bca7cfe9c33613e0af787
blessing <1424518876@qq.com>
2019-06-17 16:45:49 +0800

    Signed-off-by: blessing <1424518876@qq.com>

--- fishing/js/login.js
+++ fishing/js/login.js
@@ -1,39 +1,29 @@
 window.onload = function() {
-	alert('开始')
 console.log(typeof localStorage.getItem('wxdata'))
 //	if(!localStorage.getItem('wxdata')) {
 		var code = getUrlKey("code");
 		if(code) {
-					alert(code)
 			$.get(
 				"http://mq.soratech.cn/stores/public/game/index/openid", {
 					code: code
 				},
 				function(res) {
-									alert(res)
-
+					alert(res)
 					localStorage.setItem('wxdata', res)
 				})
 		} else {
 			getCodeApi("123");
 		}
 //	}
-	//	
 	function getUrlKey(name) { //获取url 参数
 		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null;
 	}
 
 	function getCodeApi(state) { //获取code   
-		alert(1)
 		let urlNow = encodeURIComponent(window.location.href);
-		//   alert(window.location.href)
-		alert(urlNow)
-		
 		let scope = 'snsapi_base'; //snsapi_userinfo   //静默授权 用户无感知snsapi_base
-		
 		let appid = 'wxfd06f76da71ca6db';
 		let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${urlNow}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
-		alert(2)
 		window.location.replace(url);
 	}
 
