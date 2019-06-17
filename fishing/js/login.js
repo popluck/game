c885c53d376d7b4379bd7a4270b084ab95ab2459
blessing <1424518876@qq.com>
2019-06-17 15:58:27 +0800

    Signed-off-by: blessing <1424518876@qq.com>

--- fishing/js/login.js
+++ fishing/js/login.js
@@ -4,13 +4,13 @@ window.onload = function() {
 	if(typeof wxdata == undefined) {
 		var code = getUrlKey("code");
 		if(code) {
-			//		alert(code)
+					alert(code)
 			$.get(
 				"http://mq.soratech.cn/stores/public/game/index/openid", {
 					code: code
 				},
 				function(res) {
-					//				alert(res)
+									alert(res)
 
 					localStorage.setItem('wxdata', res)
 				})
@@ -28,7 +28,7 @@ window.onload = function() {
 		//   alert(window.location.href)
 		let scope = 'snsapi_base'; //snsapi_userinfo   //静默授权 用户无感知snsapi_base
 		let appid = 'wxfd06f76da71ca6db';
-		let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${urlNow}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
+		let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${urlNow}&response_type=code&scope=${scope}#wechat_redirect`;
 		window.location.replace(url);
 	}
 
