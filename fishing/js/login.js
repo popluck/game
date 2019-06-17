f336da59cef58a4dc06d0312face0b56db374ba6
blessing <1424518876@qq.com>
2019-06-17 16:16:22 +0800

    Signed-off-by: blessing <1424518876@qq.com>

--- fishing/js/login.js
+++ fishing/js/login.js
@@ -1,22 +1,22 @@
 window.onload = function() {
 console.log(typeof localStorage.getItem('wxdata'))
-	if(!localStorage.getItem('wxdata')) {
+//	if(!localStorage.getItem('wxdata')) {
 		var code = getUrlKey("code");
 		if(code) {
-					console.log(code)
+					alert(code)
 			$.get(
 				"http://mq.soratech.cn/stores/public/game/index/openid", {
 					code: code
 				},
 				function(res) {
-									console.log(res)
+									alert(res)
 
 					localStorage.setItem('wxdata', res)
 				})
 		} else {
 			getCodeApi("123");
 		}
-	}
+//	}
 	//	
 	function getUrlKey(name) { //获取url 参数
 		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null;
