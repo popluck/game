af9d88ea0aaa80a74c30a2c6456c7023f68aa763
blessing <1424518876@qq.com>
2019-06-18 12:42:22 +0800

    Signed-off-by: blessing <1424518876@qq.com>

--- fishing/js/compound.js
+++ fishing/js/compound.js
@@ -1,11 +1,11 @@
-window.onload = function() {
+//window.onload = function() {
 	// 类型判断
 	var type = '';
 	var prize = ''; // 合成物品
 	var fishing_openid = ''; // 微信图像
 	var fishing_coupons = ''; // 优惠券
-	//	var wxdata = JSON.parse(localStorage.getItem('wxdata'))
-	//	var openid = wxdata.openid; // 用户id
+	var wxdata = JSON.parse(localStorage.getItem('wxdata'))
+	var openid = wxdata.openid; // 用户id
 	// 合成物品数量
 	var prize1 = 1;
 	var prize2 = 4;
@@ -22,15 +22,11 @@ window.onload = function() {
 	setWh('.box3')
 	setWh('.box4')
 	setWh('.box5')
-	//	console.log($('.box_0')[0])
+	
+	rePost()
 	// 合成按钮触发事件
 	$('.box_0')[0].addEventListener('click', judgeNum)
-	// 改变图标
-	judge(prize1, 'prize1', 0)
-	judge(prize2, 'prize2', 1)
-	judge(prize3, 'prize3', 2)
-	judge(prize4, 'prize4', 3)
-	judge(prize5, 'prize5', 4)
+	
 	// 设置高度
 	function setWh(res) {
 		return $(res).css('height', $('#box').width())
@@ -43,24 +39,6 @@ window.onload = function() {
 			$(res).removeClass('setAn')
 		}, 2000)
 	}
-	// 获取合成图片
-	function rePost() {
-		$.post(
-			"http://mq.soratech.cn/web_sock/public/index/index/user_sel", {
-				openid: openid
-			},
-			function(msg) {
-				if(msg) {
-					var data = JSON.parse(msg);
-					prize1 = data.prize1;
-					prize2 = data.prize2;
-					prize3 = data.prize3;
-					prize4 = data.prize4;
-					prize5 = data.prize5;
-				}
-			}
-		)
-	};
 	// 判断图标
 	function judge(res, prize, i) {
 		// 图标个数
@@ -83,12 +61,7 @@ window.onload = function() {
 			addAnimation('.box_3')
 			addAnimation('.box_4')
 			addAnimation('.box_5')
-			// 数量自减一
-			prize1--;
-			prize2--;
-			prize3--;
-			prize4--;
-			prize5--;
+			addPost()
 			// 是否切换图标
 			judge(prize1, 'prize1', 0)
 			judge(prize2, 'prize2', 1)
@@ -96,7 +69,62 @@ window.onload = function() {
 			judge(prize4, 'prize4', 3)
 			judge(prize5, 'prize5', 4)
 		} else {
-//			alert('数量不足')
+			//			alert('数量不足')
 		}
 	}
-}
+	// 获取合成需要的基础图标
+	function rePost() {
+		// 更新数据
+		$.post(
+			"http://mq.soratech.cn/web_sock/public/index/index/user_sel", {
+				openid: openid
+			},
+			function(msg) {
+				if(msg) {
+					var data = JSON.parse(msg);
+					prize1 = data.prize1;
+					prize2 = data.prize2;
+					prize3 = data.prize3;
+					prize4 = data.prize4;
+					prize5 = data.prize5;
+					// 改变图标
+	judge(prize1, 'prize1', 0)
+	judge(prize2, 'prize2', 1)
+	judge(prize3, 'prize3', 2)
+	judge(prize4, 'prize4', 3)
+	judge(prize5, 'prize5', 4)
+				}
+			}
+		)
+	};
+	// 合成物品
+	function addPost() {
+		// 添加数据
+		$.post(
+			// 请求地址
+			'http://mq.soratech.cn/web_sock/public/index/index/user_update',
+			// 数据data
+			{
+				data: [{
+					type: type,
+					prize: prize,
+					fishing_openid: fishing_openid,
+					fishing_coupons: fishing_coupons
+				}],
+				openid: openid,
+				nickname: nickname
+
+			},
+			// 成功回调函数
+			function(msg) {
+
+				// 数量自减一
+				prize1--;
+				prize2--;
+				prize3--;
+				prize4--;
+				prize5--;
+
+			})
+	};
+//}
--- fishing/js/login.js
+++ fishing/js/login.js
@@ -26,5 +26,39 @@ console.log(typeof localStorage.getItem('wxdata'))
 		let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${urlNow}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
 		window.location.replace(url);
 	}
+	
+	
+	// 钓鱼按钮
+var btn = document.querySelector('.btn');
+var gift = document.querySelector('.gift')
+btn.addEventListener('touchstart', function() {
+	btn.style.transform = 'scale(0.8)'
+})
+btn.addEventListener('touchend', function() {
+	btn.style.transform = 'scale(1)'
+})
+// 门店id
+localStorage.setItem('room',GetQueryString('id'))	
+var imgXS=1
+// 地址栏参数获取
+function GetQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r != null) return unescape(r[2]);
+	return null;
+}
+
+// 轮换
+function imgrotate(){
+	imgXS = imgXS<9?imgXS+1:1
+	$('.img1').eq(imgXS).animate({
+		'opacity':1,
+		'z-index':10
+	},150)
+	$('.img1').eq(imgXS).siblings('.img1').css({
+		'z-index':'-100',
+		'opacity':'0.5'
+	})
+}
 
 }
