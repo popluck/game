fc1143cd8ee7e936a3217efc84a000117d9b8e85
blessing <1424518876@qq.com>
2019-06-20 16:39:10 +0800

    Signed-off-by: blessing <1424518876@qq.com>

--- fishing/js/start-fishing.js
+++ fishing/js/start-fishing.js
@@ -93,8 +93,6 @@ window.onload = function() {
 			}
 		time++
 		if(H == 0) {
-			var moveMath=Math.floor(Math.random() * 3) 
-			$('.move_finsh')[0].src='image/start-fishing/鱼0'+moveMath+'.gif'
 			ws.send(JSON.stringify(sendDate))
 			
 			rodmath = 0;
